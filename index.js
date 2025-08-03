86f7219e8935a35fecdbe2d312bf48ff9d581648
Carlos Barreto <dev.carlos17@gmail.com>
2025-08-03 17:43:33 -0400

    Init project with .gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
